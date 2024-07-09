1814991a06f9495c6d6ca565d321ad45a65076a5
Van Nam Duong <z5412692@ad.unsw.edu.au>
2024-07-09 22:33:51 +1000

    contract

---
+++ src/Product.sol
@@ -0,0 +1,11 @@
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+/// @title Contract to [add yours here]
+/// @author 
+
+contract Product {
+    address public manager; //Contact manager
+
+}
