830f288955849070616de07c31379bbc85c34895
tungquanhoang <hoangtungquan22@gmail.com>
2024-07-23 05:27:46 +1000

    change managers to an array and add function to add new manager

--- src/AccessControl.sol
+++ src/AccessControl.sol
@@ -2,18 +2,44 @@
 pragma solidity 0.8.19;
 
 contract AccessControl {
-    address public manager;
+    // Array for all managers of this contract
+    address[] managers;
 
+    /// @notice constructor to create the overall access control to the product/shipment
+    /// @param _manager first manager/creator of the contract
     constructor(address _manager) {
-        manager = _manager;
+        managers.push(_manager);
     }
 
-    error Unauthorized(address caller);
+    // custom error
+    error Unauthorized(address);
 
+    /// @notice modifier to check if sender is a manager
     modifier onlyManager() {
-        if (msg.sender != manager) {
+        bool isManager = false;
+
+        for (uint i = 0; i < managers.length; i++) {
+            if (managers[i] == msg.sender) {
+                isManager = true;
+                break;
+            }
+        }
+
+        if (!isManager) {
             revert Unauthorized(msg.sender);
         }
+
         _;
+    }
+
+    /// @notice add a new manager to the contract
+    /// @param _manager new manager/creator of the contract
+    function addManager(address _manager) public onlyManager {
+        managers.push(_manager);
+    }
+
+    /// @notice get all managers of the contract
+    function getManagers() public view returns(address[] memory) {
+        return managers;
     }
 }
