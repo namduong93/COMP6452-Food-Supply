26b3eab160f8002bfeeffc9491b4032dcaf94da5
Chaw Su Thwe <chawsuthwe181@gmail.com>
2024-07-24 17:58:25 +1000

    Update: make data request more flexible

--- src/Oracle.sol
+++ src/Oracle.sol
@@ -1,70 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-import {Chainlink, ChainlinkClient} from "@chainlink/src/v0.8/ChainlinkClient.sol";
-import {ConfirmedOwner} from "@chainlink/src/v0.8/shared/access/ConfirmedOwner.sol";
-import {LinkTokenInterface} from "@chainlink/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {Chainlink, ChainlinkClient} from "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
- * THIS EXAMPLE USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
-contract Oracle is ChainlinkClient, ConfirmedOwner {
+contract WeatherOracle is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
-    int256 public volume;
+    int256 public temp;
     bytes32 private jobId;
     uint256 private fee;
 
-    event RequestVolume(bytes32 indexed requestId, int256 volume);
+    event RequestTemp(bytes32 indexed requestId, int256 temp);
 
-    /**
-     * @notice Initialize the link token and target oracle
-     *
-     * Sepolia Testnet details:
-     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-     * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
-     * jobId: ca98366cc7314957b8c012c72f05aeeb
-     *
-     */
     constructor() ConfirmedOwner(msg.sender) {
         _setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
         _setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
         jobId = "fcf4140d696d44b687012232948bdd5d";
-        fee = 0.1 * 10 ** 3; // (Varies by network and job)
+        fee = 0.1 * 10 ** 3;
     }
 
     /**
      * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     * data, then multiply by 1000 (to remove decimal places from data).
      */
-    function requestVolumeData() public returns (bytes32 requestId) {
+    function requestCurrTemp(string calldata location, string calldata data) public returns (bytes32 requestId) {
         Chainlink.Request memory req = _buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        req._add("get", "");
-        /// API KEY TO REQUEST ///
+        string memory apiLink = string.concat("https://api.weatherapi.com/v1/current.json?key=5d438b53bf2d40378b253430241507&q=", "", location);
+        req._add("get", apiLink);
 
-        // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        req._add("path", "current,temp_c"); // Chainlink nodes 1.0.0 and later support this format
+        req._add("path", data); // Chainlink nodes 1.0.0 and later support this format
 
-        // Multiply the result by 1000000000000000000 to remove decimals
+        // Multiply the result by 1000 to remove decimals
         int256 timesAmount = 10 ** 3;
         req._addInt("times", timesAmount);
 
@@ -73,11 +43,11 @@ contract Oracle is ChainlinkClient, ConfirmedOwner {
     }
 
     /**
-     * Receive the response in the form of uint256
+     * Receive the response in the form of int256
      */
-    function fulfill(bytes32 _requestId, int256 _volume) public recordChainlinkFulfillment(_requestId) {
-        emit RequestVolume(_requestId, _volume);
-        volume = _volume;
+    function fulfill(bytes32 _requestId, int256 _temp) public recordChainlinkFulfillment(_requestId) {
+        emit RequestTemp(_requestId, _temp);
+        temp = _temp;
     }
 
     /**
