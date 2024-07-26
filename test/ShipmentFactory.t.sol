445728bd9365fc1d6b17d9c9e504eb9faad1b3ea
Tung Quan Hoang <hoangtungquan22@gmail.com>
2024-07-26 22:05:01 +1000

    fix test

--- test/ShipmentFactory.t.sol
+++ test/ShipmentFactory.t.sol
@@ -25,7 +25,8 @@ contract ShipmentFactoryTest is Test {
         string[] memory locations = new string[](2);
         locations[0] = "Location1";
         locations[1] = "Location2";
-        shipmentFactory.createShipment(addr2, addr1, 100, block.timestamp, block.timestamp + 86400, locations);
+        address oracle = address(0x71D5F126bB92368c89b0469AA3D967Db14fF18D8);
+        shipmentFactory.createShipment(addr2, addr1, 100, block.timestamp, block.timestamp + 86400, locations, oracle);
         uint256[] memory shipmentCodes = registry.getShipmentCodes();
         assertEq(shipmentCodes.length, 1);
         assertEq(shipmentCodes[0], 1);
@@ -36,7 +37,8 @@ contract ShipmentFactoryTest is Test {
         string[] memory locations = new string[](2);
         locations[0] = "Location1";
         locations[1] = "Location2";
+        address oracle = address(0x71D5F126bB92368c89b0469AA3D967Db14fF18D8);
         vm.expectRevert(abi.encodeWithSelector(AccessControl.Unauthorized.selector, address(0x123)));
-        shipmentFactory.createShipment(addr2, addr1, 100, block.timestamp, block.timestamp + 86400, locations);
+        shipmentFactory.createShipment(addr2, addr1, 100, block.timestamp, block.timestamp + 86400, locations, oracle);
     }
 }
