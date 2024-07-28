2b5e1617335a42d80a2b1d72c6418c5e3bca8fec
Van Nam Duong <z5412692@ad.unsw.edu.au>
2024-07-28 17:37:37 +1000

    Finish product details

--- frontend/gui.py
+++ frontend/gui.py
@@ -1,8 +1,8 @@
-# main.py
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from frontend.product import ProductPage
 from frontend.shipment import ShipmentPage
-from frontend.registry import RegistryPage  # Import the new RegistryPage class
+from frontend.registry import RegistryPage
+from frontend.product_detail import ProductDetailPage  # Import the new ProductDetailPage class
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -25,9 +25,13 @@ class MainWindow(QMainWindow):
         self.shipment_button = QPushButton("Shipment")
         self.shipment_button.clicked.connect(self.show_shipment_page)
 
+        self.product_detail_button = QPushButton("Product Detail")
+        self.product_detail_button.clicked.connect(self.show_product_detail_page)
+
         self.main_layout.addWidget(self.product_button)
         self.main_layout.addWidget(self.shipment_button)
         self.main_layout.addWidget(self.registry_button)
+        self.main_layout.addWidget(self.product_detail_button)
 
         self.central_widget.setLayout(self.main_layout)
 
@@ -42,6 +46,10 @@ class MainWindow(QMainWindow):
     def show_registry_page(self):
         self.registry_page = RegistryPage(main_window=self)
         self.setCentralWidget(self.registry_page)
+
+    def show_product_detail_page(self):
+        self.product_detail_page = ProductDetailPage(main_window=self)
+        self.setCentralWidget(self.product_detail_page)
 
 def run_gui():
     app = QApplication([])
