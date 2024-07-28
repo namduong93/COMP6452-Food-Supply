2608fd64b10d1750e174c4e3a52432cdb8471a95
Van Nam Duong <z5412692@ad.unsw.edu.au>
2024-07-28 17:24:52 +1000

    Finish Registry

--- frontend/gui.py
+++ frontend/gui.py
@@ -1,6 +1,8 @@
+# main.py
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from frontend.product import ProductPage
-from frontend.shipment import ShipmentPage  # Import the new ShipmentPage class
+from frontend.shipment import ShipmentPage
+from frontend.registry import RegistryPage  # Import the new RegistryPage class
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,18 +16,18 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.main_layout = QVBoxLayout()
 
+        self.registry_button = QPushButton("Registry")
+        self.registry_button.clicked.connect(self.show_registry_page)
+
         self.product_button = QPushButton("Product")
         self.product_button.clicked.connect(self.show_product_page)
 
         self.shipment_button = QPushButton("Shipment")
         self.shipment_button.clicked.connect(self.show_shipment_page)
 
-        self.oracle_button = QPushButton("Oracle")
-        self.oracle_button.clicked.connect(self.show_oracle_page)
-
         self.main_layout.addWidget(self.product_button)
         self.main_layout.addWidget(self.shipment_button)
-        self.main_layout.addWidget(self.oracle_button)
+        self.main_layout.addWidget(self.registry_button)
 
         self.central_widget.setLayout(self.main_layout)
 
@@ -34,12 +36,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.product_page)
 
     def show_shipment_page(self):
-        self.shipment_page = ShipmentPage(main_window=self)  # Create and display the ShipmentPage
+        self.shipment_page = ShipmentPage(main_window=self)
         self.setCentralWidget(self.shipment_page)
 
-    def show_oracle_page(self):
-        # Implement oracle page
-        pass
+    def show_registry_page(self):
+        self.registry_page = RegistryPage(main_window=self)
+        self.setCentralWidget(self.registry_page)
 
 def run_gui():
     app = QApplication([])
